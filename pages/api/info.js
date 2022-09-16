a8e38144648f906499e6a3eae1685bef6a0f7f61
tami <xorud7979@gmail.com>
2022-09-16 17:10:03 +0900

    feat: next-js-prisma

--- pages/api/info.js
+++ pages/api/info.js
@@ -1,16 +1,82 @@
-export default function handler(req, res) {
-  if (req.method === "POST") {
-    const {
-      body: { name, age, gender },
-    } = req;
-    console.log("백엔드 로그: ", name, age, gender);
-    res.status(200).json({ data: `${name}, ${age}, ${gender}` });
-  }
+import client from "../libs/PrismaClient";
 
-  // if (req.method === "GET") {
-  //   const { name } = req.query;
-  //   res.status(200).json({ data: name });
-  // }
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    // req.body
+
+    console.log("post 호출됨");
+
+    const {
+      body: {
+        data: { name, email, password },
+      },
+    } = req;
+    console.log("백엔드 로그: ", name, email, password);
+
+    const result = await client.user.create({
+      data: {
+        name,
+        email,
+        password,
+      },
+    });
+
+    res.status(200).json(result);
+  }
+  if (req.method === "GET") {
+    console.log("get 호출됨");
+    const { email } = req.query;
+
+    const result = await client.user.findUnique({
+      select: {
+        id: false,
+        email: true,
+        name: true,
+        password: true,
+      },
+      where: {
+        email: email,
+      },
+    });
+
+    res.status(200).json({ result });
+  }
+  if (req.method === "PATCH") {
+    console.log("PATCH 호출됨");
+    const {
+      body: {
+        data: { name, email, password },
+      },
+    } = req;
+
+    console.log("데이터 파싱됨");
+    const result = await client.user.update({
+      data: {
+        name,
+        password,
+      },
+      where: {
+        email,
+      },
+    });
+    console.log("업데이트 성공");
+    res.status(200).json(result);
+  }
+  if (req.method === "DELETE") {
+    console.log("DELETE 호출됨");
+    console.log(req.body);
+    const {
+      data: { email },
+    } = req.body;
+
+    const result = await client.user.delete({
+      where: {
+        email: email,
+      },
+    });
+    console.log("삭제 완료");
+    res.status(200).json(result);
+  }
 }
 
 // 받아온 Res 인자로 여러가지 메소드를 사용할 수 있는데, 대표적인 메소드가 status이다.
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,33 @@
 import React, { useEffect } from "react";
-import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 const NextForm = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    getValues,
+  } = useForm({
+    mode: "onChange",
+  });
 
-  console.log(register("test"));
+  // const onSubmit = (data) => {
+  //   console.log(JSON.stringify(data));
+  // };
 
-  const onChangeHandler = (e) => {
-    // console.log(e.target.value);
-    setUser(e.target.value);
-  };
+  // React Hook Form 에서 e.preventDefault() 자동으로 처리해주기 때문에
+  // React HooK Form 을 사용할 경우 입력할 필요 없음
 
+  // const onChangeHandler = (e) => {
+  //   // console.log(e.target.value);
+  //   setUser(e.target.value);
+  // };
+
+  //*(data)는 아래와 mapping
+  //React Hook Form 의 {…register} 로 body: 에 JSON으로 감싼뒤 data를 입력
   const fetchUser = async (data) => {
-    e.preventDefault();
-
+    // e.preventDefault();
+    console.log(data);
     const response = await (
       await fetch("/api/info", {
         method: "POST",
@@ -22,53 +35,117 @@ const NextForm = () => {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          name: name,
+          data,
         }),
       })
     ).json();
     console.log(response);
   };
 
-  // const getUser = async () => {
-  //   const result = await (
-  //     await fetch(`/api/info?name=${user}`, {
-  //       method: "GET",
-  //     })
-  //   ).json();
-  //   console.log("result", result);
-  // };
+  const getUser = async () => {
+    console.log("getUser 호출됨");
+    const email = getValues("email");
+    const result = await (
+      await fetch(`/api/info?email=${email}`, {
+        method: "GET",
+      })
+    ).json();
+    console.log("result", result);
+  };
+
+  const update = async () => {
+    const name = getValues("name");
+    const password = getValues("password");
+    const email = getValues("email");
+
+    const response = await (
+      await fetch("/api/info", {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          data: { name: name, email: email, password: password },
+        }),
+      })
+    ).json();
+    console.log(response);
+  };
+
+  const deleteHandler = async () => {
+    const email = getValues("email");
+
+    const response = await (
+      await fetch("/api/info", {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          data: { email: email },
+        }),
+      })
+    ).json();
+    console.log(response);
+  };
 
   return (
     <>
       <form onSubmit={handleSubmit(fetchUser)}>
-        <label htmlFor="name">이름:</label>
+        <label>Name</label>
         <input
-          {...register("name")}
+          {...register("name", {
+            required: "this is required",
+            minLength: {
+              value: 4,
+              message: "Min length is 4",
+            },
+          })}
           placeholder="이름을 입력하세요."
-        />{" "}
+        />
+        {errors.name && <p>{errors.name.message}</p>}
         <br />
-        <label htmlFor="age">나이:</label>
+        <label>Email</label>
         <input
-          {...register("age")}
-          placeholder="나이를 입력하세요."
-        />{" "}
+          {...register("email", {
+            required: "this is required",
+            pattern: {
+              value:
+                /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+              message: "Invalid email address",
+            },
+          })}
+          placeholder="이메일을 입력하세요."
+        />
+        {errors.email && <p>{errors.email.message}</p>}
         <br />
-        <label htmlFor="age">성별:</label>
+        <label>Password</label>
         <input
-          {...register("gender")}
-          placeholder="성별을 입력하세요."
-        />{" "}
+          {...register("password", {
+            required: "this is required",
+          })}
+          placeholder="비밀번호를 입력하세요."
+        />
+        {errors.password && <p>{errors.password.message}</p>}
         <br />
         <div>
           <button type="submit">제출</button>
-          {/* <button onClick={getUser}>불러오기</button> */}
+          <button type="button" onClick={getUser}>
+            불러오기
+          </button>
+          <button type="button" onClick={update}>
+            수정하기
+          </button>
+          <button type="button" onClick={deleteHandler}>
+            삭제하기
+          </button>
         </div>
       </form>
       <style jsx>
         {`
           form {
             width: 500px;
-            height: 400px;
+            height: 500px;
             margin: 50px auto;
             background: #f2f2f2;
             text-align: center;
@@ -79,7 +156,7 @@ const NextForm = () => {
             width: 300px;
             height: 30px;
             border: 1px solid #ccc;
-            margin: 20px;
+            margin: 10px 20px;
             padding: 10px;
           }
           div {
@@ -101,6 +178,11 @@ const NextForm = () => {
           }
           button:nth-last-child(1) {
             background: orange;
+          }
+          p {
+            color: red;
+            margin: 0;
+            font-size: 13px;
           }
         `}
       </style>
